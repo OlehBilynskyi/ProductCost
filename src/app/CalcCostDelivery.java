09ebfd63d4ac9c34dce1d645206009115f59495c
Oleh Bilynskyi <obslinskij@gmail.com>
2025-04-18 17:13:16 +0300

    Fix CalcCostDelivery class

--- src/app/CalcCostDelivery.java
+++ src/app/CalcCostDelivery.java
@@ -3,10 +3,10 @@ package app;
 // Клас успадковує клас CalcCostBase.
 public class CalcCostDelivery {
     // Додаємо фіксовану вартість доставки
-    private final static double deliveryPrice = 7.0;
+    private final static double DELIVERY_PRICE = 7.0;
 
     //Додаємо метод для розрахунку вартості товару, з урахуванням вартості доставки
     public double calcCost(Product product) {
-        return product.getQuota() * product.getPrice() + deliveryPrice;
+        return product.getQuota() * product.getPrice() + DELIVERY_PRICE;
     }
 }
